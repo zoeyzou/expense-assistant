d15e8ee60e474b6c86a0ce2778044365339480f1
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-09-20 16:10:27 +0200

    add: add dummy data

--- src/Content/Home/Home.js
+++ src/Content/Home/Home.js
@@ -7,7 +7,7 @@ const Home = (props) => {
     <div className={styles.Home}>
       <img src={sun} alt="sun" />
       <p>Good day! Did you sleep well?</p>
-      <p>We have {props.total} expenses to work with today. Take your time!</p>
+      <p>We have <strong className={styles.emph}>{props.total}</strong> expenses to work with today. Take your time!</p>
       <p>when you are ready, go to 'all expenses' and let's rock!</p>
     </div>
   );
