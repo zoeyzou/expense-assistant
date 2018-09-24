f00733bcee8a8366a1ed23321ac025225d8ec586
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-09-24 14:33:04 +0200

    add: add file uploader and preview

--- src/Content/DataTable/Expense/FileUploader/FileUploader.js
+++ src/Content/DataTable/Expense/FileUploader/FileUploader.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import styles from './FileUploader.css';
+import ReactDropzone from 'react-dropzone';
 
-const FileUploader = (props) => {
-  return (
+class FileUploader extends Component {
+  state = {
+    files: []
+  }
+
+  onDrop = (files) => {
+    files.forEach(file => console.log(file));
+  }
+
+  onPreviewDrop = (files) => {
+    console.log(files[0]);
+    this.setState({
+      files: this.state.files.concat(files),
+     });
+  }
+
+  previewImg = (files) => {
+    if (files.length > 0) {
+      return (
+        <div>
+          {files.map((file) => (
+            <img
+              alt="Preview"
+              key={file.preview}
+              src={file.preview}
+              className={styles.imgPreview}
+            />
+        ))}
+        </div>
+      )
+    }
+  }
+
+  render () {
+
+    return (
     <React.Fragment>
-      <span>{props.title}</span>
-      <input type="file" name="file" id="file" className={styles.input} />
-      <label htmlFor="file" className={styles.FileUploader}>Choose a file AND upload</label>
-      <label></label>
+      <span>{this.props.title} </span>
+      <ReactDropzone
+        className={styles.drop}
+        accept="image/*"
+        onDrop={this.onPreviewDrop}
+      >
+        <span><i class="fas fa-upload"></i> Upload receipts</span>
+      </ReactDropzone>
+      {this.previewImg(this.state.files)}
     </React.Fragment>
+
     
   )
+    }
 };
 
 export default FileUploader;
